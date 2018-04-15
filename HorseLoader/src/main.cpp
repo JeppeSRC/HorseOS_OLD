74d52eda6755a68896c528280a51d31d81a5b401
JeppeSRC <jeppe97rc@gmail.com>
2018-04-15 09:45:32 +0200

    Working example

--- HorseLoader/src/main.cpp
+++ HorseLoader/src/main.cpp
@@ -2,7 +2,13 @@
 
 
 EFI_STATUS efi_main(EFI_HANDLE handle, EFI_SYSTEM_TABLE* systable) {
+	InitializeLibrary(handle, systable);
 
+	print(L"Booting.....");
+
+
+
+	systable->BootServices->Stall(10000000);
 
 	return EFI_SUCCESS;
 }
