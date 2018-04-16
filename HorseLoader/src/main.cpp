c0ade985b59e486128283e2e67ae19702eefccaf
JeppeSRC <jeppe97rc@gmail.com>
2018-04-16 22:13:22 +0200

    Working example that sets the text mode and prints it

--- HorseLoader/src/main.cpp
+++ HorseLoader/src/main.cpp
@@ -4,11 +4,18 @@
 EFI_STATUS efi_main(EFI_HANDLE handle, EFI_SYSTEM_TABLE* systable) {
 	InitializeLibrary(handle, systable);
 
-	print(L"Booting.....");
+
+	UINTN col = 0;
+	UINTN row = 0;
+
+	UINTN textModeIndex = GetTextMode(systable->ConOut, &col, &row);
+	systable->ConOut->SetMode(systable->ConOut, textModeIndex);
+
+	print(L"Booting.....\n");
+	printf(L"Setting text mode to %u -> %ux%u\n", textModeIndex, col, row);
 
 
-
-	systable->BootServices->Stall(10000000);
+	systable->BootServices->Stall(100000000);
 
 	return EFI_SUCCESS;
 }
